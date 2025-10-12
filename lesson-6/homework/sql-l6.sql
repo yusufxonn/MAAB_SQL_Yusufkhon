77ef7e275e911ca31ecc3e81f20d4919a1cffb62
yusufxonn <y.project.creator@gmail.com>
2025-10-12 23:25:49 +0500

    Update sql-l6.sql

--- lesson-6/homework/sql-l6.sql
+++ lesson-6/homework/sql-l6.sql
@@ -1,1 +1,84 @@
 
+SELECT DISTINCT 
+    CASE WHEN col1 < col2 THEN col1 ELSE col2 END AS col1,
+    CASE WHEN col1 < col2 THEN col2 ELSE col1 END AS col2
+FROM InputTbl;
+
+SELECT 
+    MIN(col1) AS col1,
+    MAX(col2) AS col2
+FROM InputTbl
+GROUP BY 
+    CASE WHEN col1 < col2 THEN col1 ELSE col2 END,
+    CASE WHEN col1 < col2 THEN col2 ELSE col1 END;
+
+
+
+CREATE TABLE TestMultipleZero (
+    A INT NULL,
+    B INT NULL,
+    C INT NULL,
+    D INT NULL
+);
+
+INSERT INTO TestMultipleZero(A,B,C,D)
+VALUES 
+    (0,0,0,1),
+    (0,0,1,0),
+    (0,1,0,0),
+    (1,0,0,0),
+    (0,0,0,0),
+    (1,1,1,0);
+
+select * from TestMultipleZero
+
+
+ 
+select 
+        case 
+               when A = 1 or B = 1 or C = 1 or D = 1 then 1
+        end as result
+        from TestMultipleZero
+        where A = 1 or B = 1 or C = 1 or D = 1
+
+
+create table section1(id int, name varchar(20))
+insert into section1 values (1, 'Been'),
+       (2, 'Roma'),
+       (3, 'Steven'),
+       (4, 'Paulo'),
+       (5, 'Genryh'),
+       (6, 'Bruno'),
+       (7, 'Fred'),
+       (8, 'Andro')
+
+select * from section1
+where id % 2 = 1
+
+select * from section1
+where id = (select MIN(id) from section1)
+
+select * from section1
+where id = (select MAX(id) from section1)
+
+select * from section1
+where name like 'b%'
+
+CREATE TABLE ProductCodes (
+    Code VARCHAR(20)
+);
+
+INSERT INTO ProductCodes (Code) VALUES
+('X-123'),
+('X_456'),
+('X#789'),
+('X-001'),
+('X%202'),
+('X_ABC'),
+('X#DEF'),
+('X-999');
+
+select * from ProductCodes
+where Code like '%\_%'
+escape '\'  
+
